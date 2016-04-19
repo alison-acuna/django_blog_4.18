fc94bbb7c8c78827263d88aff2be04c5c3d949cc
alison-acuna <aacuna136@gmail.com>
2016-04-18 20:21:24 -0700

    1st Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alisonacuna/projectsnightclass/django_blog_4.18/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

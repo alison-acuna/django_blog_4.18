e0ff4113a6e37cf7c4f19e4664b3b3062800c4e4
alison-acuna <aacuna136@gmail.com>
2016-04-21 20:06:10 -0700

    Templates Added

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alisonacuna/projectsnightclass/django_blog_4.18/venv/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
